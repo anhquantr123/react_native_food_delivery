dc030ce923493f907f532562f71395b98acd8bd7
NGUYỄN VĂN QUÂN <anhquan.lien123@gmail.com>
2021-08-26 01:35:12 +0700

    stack screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import 'react-native-gesture-handler';
+import { SafeAreaView, StyleSheet, Text, StatusBar  , View} from "react-native";
+import HomeScreen from "./src/screens/home_screen/HomeScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+const bottomTab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={style.container}>
+      <NavigationContainer>
+      <Stack.Navigator screenOptions ={{
+        headerShown : false      }}
+        initialRouteName={"Home"}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    <StatusBar barStyle="default"/>
+    </SafeAreaView>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style  = StyleSheet.create({
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
+    backgroundColor: '#ffffff'
+  }
+
+
 });
+
+
+
---
+++ src/screens/order/OrderDelivery.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+
+export default function OrderDelivery() {
+    return (
+        <SafeAreaView>
+            <Text></Text>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
